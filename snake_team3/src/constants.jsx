ffc80795641a7b2813b4c9341eaef1b6f8ff943d
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-14 16:26:09 +0200

    use WASD keys, 3 diff levels

--- snake_team3/src/constants.jsx
+++ snake_team3/src/constants.jsx
@@ -1,36 +1,39 @@
 const board_size = [500, 500];
 
 const start_snake = [
-    [3, 15],
-    [3, 16],  
-    [3, 17]  
+  [3, 8],
+  [3, 9],
+  [3, 10],
 ];
 
-const start_apple = [6, 2]
+const start_apple = [6, 2];
 
-
-const square_size = 30;
-
-const snake_speed = 400;
+// difficulty level by speed and scaling
+const difficulty = {
+  easy: {
+    speed: 500,
+    size: 40,
+  },
+  medium: {
+    speed: 350,
+    size: 30,
+  },
+  hard: {
+    speed: 200,
+    size: 20,
+  },
+};
 
 // Arrow keys and gaming code configation (WASD)
 const movement = {
-    38: [0, -1],
-    40: [0, 1],
-    37: [-1, 0],
-    39: [1, 0],
-    65: [-1, 0],
-    83: [0, 1],
-    68: [1, 0],
-    87: [0, -1]
+  38: [0, -1],
+  40: [0, 1],
+  37: [-1, 0],
+  39: [1, 0],
+  65: [-1, 0],
+  83: [0, 1],
+  68: [1, 0],
+  87: [0, -1],
 };
 
-export {
-    board_size,
-    start_snake,
-    start_apple,
-    square_size,
-    snake_speed,
-    movement
-};
-
+export { board_size, start_snake, start_apple, movement, difficulty };
